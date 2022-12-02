3f650372d71175ab67aee9971a91fa4c2965c232
viviani22 <vivianxhumetiku@gmail.com>
2022-12-02 11:50:27 -0800

    task 2

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""returns information about his/her TODO list progress"""
+import urllib.request
+import json
+import sys
+
+
+if __name__ == '__main__':
+
+    uId = int(sys.argv[1])
+    userDict = {}
+    taskList = []
+    with urllib.request.urlopen('https://jsonplaceholder'
+                                '.typicode.com/users') as response:
+        users = json.loads(response.read().decode())
+        for user in users:
+            if user['id'] == uId:
+                userName = user['username']
+
+    with urllib.request.urlopen('https://jsonplaceholder'
+                                '.typicode.com/todos') as response:
+        jsonDict = json.loads(response.read().decode())
+        for line in jsonDict:
+            if line['userId'] == uId:
+                tempDict = {"task": line['title'], "completed": line['completed'], "username": userName}
+                taskList.append(tempDict)
+            userDict[uId] = taskList
+
+
+    with open(f'./{uId}.json', 'w+') as file:
+        file.write(json.dumps(userDict))
