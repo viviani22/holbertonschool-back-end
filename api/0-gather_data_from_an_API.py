990ada2bc6d3a5675090dbe402b761086302fff8
viviani22 <vivianxhumetiku@gmail.com>
2022-12-02 10:33:07 -0800

    task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+For a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import urllib.request
+import json
+import sys
+
+if __name__ == "__main__":
+    uId = int(sys.argv[1])
+    toDoList = []
+    compTask = 0
+    tasks = 0
+    with urllib.request.urlopen('https://jsonplaceholder'
+                                '.typicode.com/users') as response:
+        users = json.loads(response.read().decode())
+        for user in users:
+            if user['id'] == uId:
+                userName = user['name']
+
+    with urllib.request.urlopen('https://jsonplaceholder'
+                                '.typicode.com/todos') as response:
+        jsonDict = json.loads(response.read().decode())
+        for line in jsonDict:
+            if line['userId'] == uId:
+                if line['completed'] is True:
+                    string = ("\t " + line['title'])
+                    toDoList.append(string)
+                    compTask += 1
+                tasks += 1
+
+    print(f"Employee {userName} is done with tasks({compTask}/{tasks}):")
+    for att in toDoList:
+        print(att)
